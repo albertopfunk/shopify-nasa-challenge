afeb0492f9d01694db125d04987d9e3022507c31
Alberto-Funk <laptopreneur@gmail.com>
2021-09-21 15:00:51 -0700

    initial structure - setting up initial HTML structure - I have an idea of what to do and already started pseudo code on trello - setting up project as a static page and will hydrate with API - will be a feed of nasa article cards

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,92 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		// check local storage
+		// initial data
+		console.log("HELLO");
+	});
+</script>
+
+<svelte:head>
+	<title>NASA Images</title>
+</svelte:head>
+
+<div class="main-container">
+	<main>
+		<h1>NASA to the Moon!</h1>
+
+		<div
+			class="nasa-cards-feed"
+			role="feed"
+			aria-busy="ISLOADING"
+			aria-labelledby="cards-heading"
+		>
+			<h2 id="cards-heading">Cards Title</h2>
+
+			<article
+				id="nasa-card-INDEX"
+				class="nasa-card"
+				tabIndex="0"
+				aria-posinset="INDEX+1"
+				aria-setsize="TOTAL_CARDS"
+				aria-labelledby="card-INDEX-heading"
+				aria-describedby="IMG_ALT"
+			>
+				<h3 id="card-INDEX-heading">Card Title</h3>
+				<p>Card subtitle</p>
+
+				<img src="SRC" alt="ALT" />
+
+				<h4>Description Title</h4>
+
+				<p>Date</p>
+
+				<p>Description</p>
+
+				<button type="button">
+					<span>visually hidden "like image"</span>
+					<span>like icon</span>
+				</button>
+			</article>
+		</div>
+	</main>
+
+	<footer>
+		<p>Built with NASA by Alberto Preciado</p>
+	</footer>
+</div>
+
+<style>
+	.main-container {
+		min-height: 100vh;
+		padding: 0 0.5rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: stretch;
+	}
+
+	main {
+		padding: 0.5rem 0;
+	}
+
+	footer {
+		flex-shrink: 0;
+		width: 100%;
+		height: 75px;
+		border-top: 1px solid #eaeaea;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+  .nasa-card {
+    border: 1px solid #eaeaea;
+    border-radius: 10px;
+  }
+</style>
