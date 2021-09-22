5525ccd38017abfa7d54d69813594662f2b5e3e0
Alberto-Funk <laptopreneur@gmail.com>
2021-09-21 20:00:13 -0700

    setting up deployment - using netlify - adding adapter and updating config for prerender

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,17 @@
+import adapter from "@sveltejs/adapter-netlify";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: "#svelte",
+		prerender: {
+			crawl: true,
+			enabled: true,
+			onError: "continue",
+			pages: ["*"]
+		}
 	}
 };
 
