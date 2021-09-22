525eb456f2831a6a35cf2b5f5e4751ddec058b57
Alberto-Funk <laptopreneur@gmail.com>
2021-09-22 03:37:13 -0700

    spacer - adding spacer to avoid margins

---
+++ src/lib/Spacer.svelte
@@ -0,0 +1,42 @@
+<script>
+	export let size = "xsm";
+</script>
+
+<span
+	class:xsm={size === "xsm"}
+	class:sm={size === "sm"}
+	class:md={size === "md"}
+	class:lg={size === "lg"}
+	class:xlg={size === "xlg"}
+/>
+
+<style lang="scss">
+	span {
+		display: block;
+	}
+
+	.xsm {
+		height: 5px;
+		min-height: 5px;
+	}
+
+	.sm {
+		height: 10px;
+		min-height: 10px;
+	}
+
+	.md {
+		height: 15px;
+		min-height: 15px;
+	}
+
+	.lg {
+		height: 20px;
+		min-height: 20px;
+	}
+
+	.xlg {
+		height: 30px;
+		min-height: 30px;
+	}
+</style>
